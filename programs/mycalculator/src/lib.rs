7273afd54c3c4f0ee24f56f483b4a4eebf14d420
nicolskiprop <yegonicholas@gmail.com>
2022-04-11 15:01:09 +0300

    calculator

--- programs/mycalculator/src/lib.rs
+++ programs/mycalculator/src/lib.rs
@@ -6,10 +6,16 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod mycalculator {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>, init_message: String) -> programResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
         Ok(())
     }
+
+    // pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    //     Ok(())
+    // }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+// #[derive(Accounts)]
+// pub struct Initialize {}
